297a7b246fc2738f5cc6f2bc07adc2daf54fa69c
Nagibe Maroun-Gonzalez <nagibe.maroun@gmail.com>
2026-02-01 22:31:08 +0100

    Implemented regridding and zarr storeage as well as plotting

--- ERA5_Data/RealDailyRoutineERA5.py
+++ ERA5_Data/RealDailyRoutineERA5.py
@@ -1,13 +1,16 @@
-import sys
 import os
 import cdsapi
 from datetime import datetime, timedelta
 import numpy as np
 import xarray as xr 
-#import healpy as hp
+import healpy as hp
+import matplotlib.pyplot as plt
+from healpy.newvisufunc import projview
+import json
+import urllib.request
 
  
-StoreZARR = "era5_humidity.zarr"
+StoreZARR = "era5_precipitation.zarr"
 
 # real routines
 
@@ -31,16 +34,16 @@ def real_daily_data_routine(date,variable, stat):
         print(f"Downloading {variable} for {date_str}...")
         download_era5(date, variable, stat, tmp_file)
         
-        print("Regridding to HEALPix (NSIDE 8 and 16).") 
-        #regridded_data = regrid_to_healpix(tmp_file)
+        print("Regridding to HEALPix (NSIDE 128).") 
+        regridded_data = regrid_to_healpix(tmp_file)
         
         
         print(f"Appending to Zarr store: {StoreZARR}") 
-        #save_to_zarr(regridded_data, StoreZARR, date_str)
+        save_to_zarr(regridded_data, StoreZARR, date_str)
     
         # mark as done
-        #with open(f"processed_{date.strftime('%Y%m%d')}.txt", "w") as f:
-        #    f.write("done")
+        with open(f"processed_{date.strftime('%Y%m%d')}.txt", "w") as f:
+            f.write("done")
             
         #clean up the temporary NetCDF file to save space
         if os.path.exists(tmp_file):
@@ -76,7 +79,8 @@ def download_era5(date, variable, stat, targetFile):
 
 # regridding routine
 
-def regrid_to_healpix(source_file, nside_list=[8, 16]):
+
+def regrid_to_healpix(source_file, nside_list=[128]):
     ds = xr.open_dataset(source_file)
     
     if 'valid_time' in ds.dims:
@@ -96,6 +100,9 @@ def regrid_to_healpix(source_file, nside_list=[8, 16]):
         # convert to degrees and lat/long
         target_lats = 90.0-np.degrees(theta)
         target_lons = np.degrees(phi)
+        # align Longitude Conventions - convert our target (0-360) to match (-180 to 180).
+        if ds.longitude.min() < 0:
+            target_lons = np.where(target_lons > 180, target_lons - 360, target_lons)
         
         # INTERPOLATE
         # xarray's .interp(), pass the new target coordinates.
@@ -142,6 +149,71 @@ def save_to_zarr(data_dict, store_path, date_str):
             
 
     print(f"Data for {date_str} saved to {store_path}")
+    
+    
+# plot routine
+
+
+def add_mexico_border_standard():
+    try:
+        url = "https://raw.githubusercontent.com/johan/world.geo.json/master/countries/MEX.geo.json"
+        with urllib.request.urlopen(url) as response:
+            data = json.load(response)
+        
+        geom = data['features'][0]['geometry']
+        coords_list = [geom['coordinates']] if geom['type'] == 'Polygon' else geom['coordinates']
+        
+        for poly in coords_list:
+            ring = np.array(poly[0])
+            plt.plot(ring[:, 0], ring[:, 1], 'k-', linewidth=1.5, alpha=1.0, zorder=10)
+    except Exception as e:
+        print(f"Could not load coastline: {e}")
+
+def plot_precip_scatter(store_path, target_date, nside_val=128):
+    nside_group = f"NSIDE_{nside_val}"
+    ds_zarr = xr.open_zarr(store_path, group=nside_group, consolidated=True)
+    
+    try:
+        sample = ds_zarr.sel(time=target_date, method='nearest')
+    except KeyError:
+        print("Date not found, using first index.")
+        sample = ds_zarr.isel(time=0)
+
+    # get Data
+    precip_mm = sample['tp'].values * 1000
+    precip_mm = np.nan_to_num(precip_mm, nan=0.0)
+    print(f"Max Precip Value: {precip_mm.max():.2f} mm") 
+    # coordinate calculation
+    npix = len(precip_mm)
+    theta, phi = hp.pix2ang(nside_val, np.arange(npix))
+    lats = 90.0 - np.degrees(theta)
+    lons = np.degrees(phi)
+    lons = (lons + 180) % 360 - 180 # Force -180 to 180
+
+    # Mexico region and min precip Box: [-118, -86, 14, 33]
+    mask = (lons >= -120) & (lons <= -85) & (lats >= 12) & (lats <= 35) & (precip_mm > 0.05)
+    
+    plot_lons = lons[mask]
+    plot_lats = lats[mask]
+    plot_data = precip_mm[mask]
+
+    # plot
+    fig, ax = plt.subplots(figsize=(10, 8))
+    sc = ax.scatter(plot_lons, plot_lats, c=plot_data, cmap="Blues", 
+                    vmin=0, vmax=60, s=65, marker='o', edgecolors='none')
+    add_mexico_border_standard()
+    cbar = plt.colorbar(sc, ax=ax, orientation='horizontal', pad=0.2, shrink=0.9)
+    cbar.set_label("Precipitation (mm)", fontsize=12)
+
+    ax.set_title(f"Total Precipitation | {target_date} | NSIDE {nside_val}", fontsize=14)
+    ax.set_xlim([-118, -86])
+    ax.set_ylim([14, 33])
+    ax.set_xlabel("Longitude")
+    ax.set_ylabel("Latitude")
+    ax.grid(True, linestyle='--', alpha=0.5)
+    
+    plt.show()
+
 
 # control flow
 
