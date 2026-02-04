381857130518282eae0a82d331ca43b59b2c15d9
Johanna <johanna@Johannas-MBP.fritz.box>
2026-02-04 23:32:13 +0100

    clean up code

---
+++ GPM/scripts/runIMERG.py
@@ -0,0 +1,8 @@
+import ControlFlowIMERG as cf
+
+### PARAMETER TO CHANGE ###
+start = "2014-05-01 00:00:00"
+end = "2014-05-31 00:00:00"
+
+
+cf.daily_routine(start, end, nside=128)
---
+++ GPM/scripts/statistics.py
@@ -0,0 +1,64 @@
+import xarray as xr
+import healpy as hp
+import numpy as np
+import pandas as pd
+
+def compute_precip_stats(
+    store_path,
+    nside=128,
+    lat_min=14.0, lat_max=33.0,
+    lon_min=-118.0, lon_max=-86.0
+):
+
+    # Load data
+    ds = xr.open_zarr(store_path, consolidated=True)
+    precip = ds["precipitation"]
+
+    # HEALPix geometry
+    npix = hp.nside2npix(nside)
+    pixels = np.arange(npix)
+    theta, phi = hp.pix2ang(nside, pixels)
+
+    lats = 90.0 - np.degrees(theta)
+    lons = np.degrees(phi)
+    lons = np.where(lons > 180, lons - 360, lons)
+
+    # Regional mask
+    mask = (
+        (lats >= lat_min) & (lats <= lat_max) &
+        (lons >= lon_min) & (lons <= lon_max)
+    )
+
+    print(f"Pixels in region: {mask.sum()}")
+
+    # -------------------------------------------------
+    # DAILY STATISTICS (explicit but correct)
+    # -------------------------------------------------
+    daily_records = []
+
+    for t in range(precip.sizes["time"]):
+        data = precip.isel(time=t).values[mask]
+
+        valid = ~np.isnan(data)
+
+        daily_records.append({
+            "date": pd.Timestamp(precip.time.values[t]).date(),
+            "min_mm":  np.nanmin(data[valid]),
+            "max_mm":  np.nanmax(data[valid]),
+            "mean_mm": np.nanmean(data[valid]),
+        })
+
+    df_daily = pd.DataFrame(daily_records)
+    df_daily.to_csv("../data/daily_precip_stats.csv", index=False)
+
+    return df_daily
+
+
+store_path = "../ZARR/IMERG_PRECIP_nside128.zarr"
+
+compute_precip_stats(
+    store_path,
+    nside=128,
+    lat_min=14.0, lat_max=33.0,
+    lon_min=-118.0, lon_max=-86.0
+)
