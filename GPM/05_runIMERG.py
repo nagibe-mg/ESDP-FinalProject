7b4039f990d0813419572d9110f053099aa8a8e4
Johanna <johanna@johannas-mbp.speedport.ip>
2026-02-04 09:43:19 +0100

    some changes

--- GPM/05_runIMERG.py
+++ GPM/05_runIMERG.py
@@ -10,7 +10,7 @@ import os
 ## trigger download of IMERG data
 ### PARAMETER TO CHANGE ###
 start = "2014-05-01 00:00:00"
-end = "2014-05-02 00:00:00"
+end = "2014-05-31 00:00:00"
 #lat_min, lat_max = 14.0, 33.0  
 #lon_min, lon_max = -118.0, -86.0
 #region = [lat_min, lat_max, lon_min, lon_max]
